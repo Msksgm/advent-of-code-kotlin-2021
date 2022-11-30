f03f667290a99aeadd064381b64f06b30e185536
Msksgm <msksgm3124@gmail.com>
2022-12-01 08:34:46 +0900

    feat: answer of Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { (a, b) -> a < b }
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<Int>): Int {
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInputAsInts("Day01_test")
+    // check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInputAsInts("Day01")
     println(part1(input))
     println(part2(input))
 }
